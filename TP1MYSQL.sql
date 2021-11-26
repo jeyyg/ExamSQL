fdc8d0ec5ed55b16685b193c3cd636bacfbca8dd
Jeyyg <81153547+jeyyg@users.noreply.github.com>
2021-11-26 12:41:59 +0100

    Update TP1MYSQL.sql

--- TP1MYSQL.sql
+++ TP1MYSQL.sql
@@ -21,11 +21,12 @@ GROUP BY compagny;
 /*01.3 Quels sont les pilots qui sont au-dessus du salaire moyen.*/
 SELECT name, salary
 FROM pilots
-WHERE salary > (SELECT AVG(salary) FROM pilots);
+WHERE salary > (SELECT AVG(salary)
+    FROM pilots);
 
 
 /*01.4 Calculez l'étendu des salaires.*/
-SELECT MAX(salary) - MIN(salary) AS etendu_des_dalaires
+SELECT MAX(salary) - MIN(salary) AS etendu_des_salaires
 FROM pilots;
 
 /*01.5 Quels sont les noms des compagnies qui payent leurs pilotes au-dessus de la moyenne ?*/
@@ -33,14 +34,16 @@ SELECT c.name, p.salary
 FROM compagnies AS c
 INNER JOIN pilots AS p
 ON c.comp = p.compagny
-WHERE p.salary > (SELECT AVG(salary) FROM pilots);
+WHERE p.salary > (SELECT AVG(salary)
+    FROM pilots);
 
 /*01.6 Quels sont les pilotes qui par compagnie dépasse(nt) le salaire moyen ?*/
 SELECT p.name, p.salary, c.name
 FROM pilots as p
 INNER JOIN compagnies as c
 ON p.compagny = c.comp
-WHERE salary > (SELECT AVG(salary) FROM pilots)
+WHERE salary > (SELECT AVG(salary)
+    FROM pilots)
 GROUP BY c.name;
 
 
@@ -52,15 +55,15 @@ WHERE compagny = 'AUS';
 /*02.2 Vérifiez que les salaires des pilotes australiens sont tous inférieurs aux autres salaires des pilotes des autres compagnies.*/
 SELECT salary, compagny
 FROM pilots
-WHERE salary = (compagny = "AUS") < ALL(SELECT salary FROM pilots)
+WHERE salary = (compagny = "AUS") < ALL(SELECT salary 
+    FROM pilots)
 GROUP BY compagny;
 
 /*03.1 On aimerait savoir quels sont les types d'avions en commun que la compagnie AUS et FRE1 exploitent.*/
 SELECT DISTINCT plane, compagny
 FROM pilots
 WHERE compagny='AUS'
-AND plane IN (
-  SELECT plane
+AND plane IN (SELECT plane
   FROM pilots
   WHERE compagny='FRE1');
 
